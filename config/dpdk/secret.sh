94d88c72e1d2ea8fe484147a3ce2e23212170b68
karmab <karimboumedhel@gmail.com>
2020-09-21 17:40:56 +0200

    seconf commit

--- config/dpdk/secret.sh
+++
@@ -1,8 +0,0 @@
-SECRET='redhat-io-secret'
-REGISTRY='registry.redhat.io'
-USERNAME='XXX'
-PASSWORD='YYY'
-MAIL="ZZZ"
-oc create secret docker-registry $SECRET --docker-server=$REGISTRY --docker-username=$USERNAME --docker-password=$PASSWORD --docker-email=$MAIL
-oc secrets link default $SECRET --for=pull
-oc secrets link builder $SECRET --for=pull
